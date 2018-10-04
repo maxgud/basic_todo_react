68cd914a4b58018e2bead9aa41be69cb0fb84cd4
Max Gudmundson <maxgud@gmail.com>
2018-10-03 18:40:17 -0600

    basic todo

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+class App extends Component {
+  state = { todos: [] }
+
+  componentDidMount() {
+    //TODO make a call to our rails server to get Items
+  }
+
+addItem = (name) => {
+  //TODO make api call to rails server to add item
+  const { todos } = this.state;
+  //Generate random id
+  const id = Math.floor(( 1 + Math.random()) * 0x1000).toString()
+  this.setState({ todos: [...todos, { id, name }] });
+}
+
+  updateTodo = (id) => {
+    //TODO make api call to update todo
+    //TODO update state
+  }
+
+  deleteTodo = (id) => {
+    //TODO make api call to delete todo
+    //TODO remove it from state
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <TodoForm addItem={this.addItem} />
+        <TodoList
+          todos={this.state.todos}
+          updateTodo={this.updateTodo}
+          deleteTodo={this.deleteTodo}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
